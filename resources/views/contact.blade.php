40669a1783aa523564f7bf7da9b88625f6fe6e8f
Rifqi Harits <rharits18@gmail.com>
2024-10-28 23:06:32 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+          <a href="https://instagram.com/rfkharits/">instagram</a>
+        </li>
+        <li>
+            <a href="https://facebook.com/harits077/">facebook</a>
+        </li>
+        <li>
+            <a href="https://id.pinterest.com/Thisthv18/">Pinterest</a>
+        </li>
+        <li>
+            <a href="https://youtube.com/rifqiharitspratama157">Youtube</a>
+        </li>
+    </ul>
+</body>
+</html>
